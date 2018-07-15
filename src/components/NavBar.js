06b1186d9ed120f53d9c83b3af197d3598cd0fac
DanielJBailey <Dbailey0210@gmail.com>
2018-07-15 13:19:12 -0500

    finish base navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import '../styles/NavBar.css';
 
 const NavBar = () => {
     
 
     return (
         <div className="nav-bar">
+            <div class="logo">
             <h1>Wad</h1>
+            <p>finding a place to store it.</p>
+            </div>
+            
+            <ul>
+                <li>Log In</li>
+                <li>Register</li>
+            </ul>
         </div>
     );
 
