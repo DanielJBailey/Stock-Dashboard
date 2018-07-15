0a6fd038ce2503701802ab48825162868f08b526
DanielJBailey <Dbailey0210@gmail.com>
2018-07-15 12:58:28 -0500

    added sass dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       
+        <NavBar />
 
-
-       
       </div>
     );
   }
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+    
+
+    return (
+        <div className="nav-bar">
+            <h1>Wad</h1>
+        </div>
+    );
+
+};
+
+
+
+export default NavBar;
